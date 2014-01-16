d2a1aa63f660816814a85fb02fc3fcf4e8aa9b33
Avishay <avishorp@gmail.com>
2014-01-16 08:17:17 +0200

    Add discrete segment constants and some more tests

--- TM1637Display.h
+++ TM1637Display.h
@@ -19,6 +19,14 @@
 
 #include <inttypes.h>
 
+#define SEG_A   0b00000001
+#define SEG_B   0b00000010
+#define SEG_C   0b00000100
+#define SEG_D   0b00001000
+#define SEG_E   0b00010000
+#define SEG_F   0b00100000
+#define SEG_G   0b01000000
+
 class TM1637Display {
 
 public:
